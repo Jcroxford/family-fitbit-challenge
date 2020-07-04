dc1d84ccdf471c126f5811d19000a1eb1b910bcf
Jcroxford <takeflightsc@gmail.com>
2020-07-04 02:22:00 -0600

    init fitbit passport strategy middelware and auth routes

--- src/controllers/fitbit.controller.js
+++ src/controllers/fitbit.controller.js
@@ -1,9 +1,23 @@
 import express from 'express'
 
+import passport from 'passport'
+import '../middleware/passport/fitibitStrategy.js'
+
 const router = express.Router()
 
-router.post('/webhook', (req, res) => {
-  console.log(req.body)
+router.get('/auth', passport.authenticate('fitbit', { scope: ['activity', 'heartrate', 'location', 'profile'] }))
+
+router.get('/auth/callback', passport.authenticate('fitbit', {
+  successRedirect: '/api/v1/fitbit/auth/success',
+  failureRedirect: '/api/v1/fitbit/auth/failure'
+}))
+
+router.get('/auth/success', (req, res) => {
+  res.json({ success: true, errors: null })
+})
+
+router.get('/auth/failure', (req, res) => {
+  res.json({ success: false, errors: null })
 })
 
 export default router
---
+++ src/middleware/passport/fitibitStrategy.js
@@ -0,0 +1,27 @@
+import passport from 'passport'
+import FitbitPassport from 'passport-fitbit-oauth2'
+
+const FitbitOAuth2Strategy = FitbitPassport.FitbitOAuth2Strategy
+
+passport.use(new FitbitOAuth2Strategy(
+  {
+    clientID: process.env.FITBIT_CLIENT_ID,
+    clientSecret: process.env.FITBIT_CLIENT_SECRET,
+    callbackURL: `${process.env.SERVER_ROOT_ADDRESS}/api/v1/fitbit/auth/callback`
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken: ', accessToken)
+    console.log('refreshToken: ', refreshToken)
+    console.log('profile: ', profile)
+
+    done(null, { messages: [] })
+  }
+))
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
