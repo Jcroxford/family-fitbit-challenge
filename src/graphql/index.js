d618689861746f5254f571b9ce94dfd977429846
Jcroxford <takeflightsc@gmail.com>
2020-07-12 19:44:33 -0600

    add date scalar type

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,12 @@
 const { makeExecutableSchema } = require('apollo-server-express')
+const merge = require('lodash.merge')
 
 const typeDefs = require('./types')
 const resolvers = require('./resolvers')
 
-module.exports = makeExecutableSchema({ typeDefs, resolvers })
+const { DateType, DateTypeResolver } = require('./customScalars/DateType')
+
+module.exports = makeExecutableSchema({
+  typeDefs: [...typeDefs, DateType],
+  resolvers: merge(resolvers, DateTypeResolver)
+})
