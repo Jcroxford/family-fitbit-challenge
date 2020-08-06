7a56dce4c1b5faf64eef79e5b463ef88139e3a49
Jcroxford <takeflightsc@gmail.com>
2020-08-05 23:44:55 -0600

    added create user & fitbit user to fitbit strategy

--- src/middleware/passport/fitibitStrategy.js
+++ src/middleware/passport/fitibitStrategy.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
 const FitbitOAuth2Strategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy
 
+const db = require('../../models')
+
 passport.use(new FitbitOAuth2Strategy(
   {
     clientID: process.env.FITBIT_CLIENT_ID,
@@ -8,11 +10,30 @@ passport.use(new FitbitOAuth2Strategy(
     callbackURL: `${process.env.SERVER_ROOT_ADDRESS}/api/v1/fitbit/auth/callback`
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken: ', accessToken)
-    console.log('refreshToken: ', refreshToken)
-    console.log('profile: ', profile)
+    console.log('[FITBIT STRATEGY] strating auth strat for fitbit')
+    db.User
+      .query()
+      .findOne({ firstName: profile._json.user.firstName, lastName: profile._json.user.lastName })
+      .withGraphFetched('fitbitUser')
+      .then(user => {
+        if (user) return user
 
-    done(null, { messages: [] })
+        console.log('[FITBIT STRATEGY] existing user not found. creating new user')
+        return db.User.query().insertGraph({
+          firstName: profile._json.user.firstName,
+          lastName: profile._json.user.lastName,
+          fitbitUser: {
+            fitbitId: profile.id,
+            accessToken,
+            refreshToken
+          }
+        })
+      })
+      .then(user => done(null, { user }))
+      .catch(err => {
+        console.log('[FITBIT STRATEGY] strategy failed', err)
+        done(err, null)
+      })
   }
 ))
 
